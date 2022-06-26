7b5c57498bff4e855337417131e7660f6bd1c356
GulshanKumar-cod <68959184+GulshanKumar-cod@users.noreply.github.com>
2022-06-26 13:09:27 +0530

    Updates

---
+++ precache-manifest.90af59cd044b1d236c3e25b56ddaa167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f3152155d6545dd443f",
+    "url": "/textutils/static/css/main.028b5a00.chunk.css"
+  },
+  {
+    "revision": "5f3152155d6545dd443f",
+    "url": "/textutils/static/js/main.5f315215.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "9f2b682b3efb10466c9270562887245f",
+    "url": "/textutils/index.html"
+  }
+];
